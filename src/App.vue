cca8ba27f8afecdaaf07c6508b7208c5bf075654
carlosescor2199 <carlos.escudero.corpas@gmail.com>
2020-12-02 22:01:20 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <NavBar />
+    <TeamCard />
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar';
+import TeamCard from './components/TeamCard'
+export default {
+  name: 'App',
+  components: {
+    TeamCard,
+    NavBar
+  }
+}
+</script>
+
