69290d5d8923199b656334ce0211aa754ddbdbb4
roalmato <ramartinez605@misena.edu.co>
2020-12-04 23:36:33 -0500

    Add componet Fotter

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <NavBar />
     <TeamCard />
+    <FooterPag/>
   </div>
 </template>
 
 <script>
 import NavBar from './components/NavBar';
-import TeamCard from './components/TeamCard'
+import TeamCard from './components/TeamCard';
+import FooterPag from './components/FooterPag';
 export default {
   name: 'App',
   components: {
     TeamCard,
-    NavBar
+    NavBar,
+    FooterPag,
   }
 }
 </script>
