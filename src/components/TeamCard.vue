60f8da62b381b3eccfda60824a3071cc067c9322
Viviana Chacon <apple@MacBook-Pro-de-Apple.local>
2020-12-05 00:39:47 -0500

    Add TeamCard

--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,16 +1,57 @@
 <template>
-    <div>
-
+    <div class="card bg-dark">
+      <div class="card bg-light">
+        <span class="border border-secondary">
+            <img class="mx-auto d-block " :src= "imagen"/>
+            <div class="card-body">
+               <h5 class="card-title">{{nombre}}</h5>
+               <p class="card-text " >
+               <p class="text-justify">    
+                   {{descripcion}}
+               </p>
+               <p class="card-text font-italic"><small>Rol: {{rol}}</small></p>
+            </div>
+            </span>
+        </div>   
     </div>
 </template>
 
 <script>
     export default {
         name: "TeamCard",
-        props: ['member']
+        props: {
+            codigo: {
+                type: Number,
+                required: true,
+
+            },
+            nombre: {
+                type: String,
+                required: true,
+
+            },
+            descripcion: {
+                type: String,
+                required: true,
+
+            },
+            rol: {
+                type: String,
+                required: true,
+
+            },
+             imagen: {
+                type: String,
+                required: true,
+
+            }
+
+        }
     }
+
 </script>
 
 <style scoped>
+
 
 </style>
