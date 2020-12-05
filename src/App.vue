c5ac5dd44df1cb0ca97765c25b5a01d3338bce0d
jucamilof <juancamilfonseca603@gmail.com>
2020-12-05 01:56:43 -0500

    Services

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+
+<template>
+  <div id="app">
+    <NavBar />
+    <TeamCard />
+    <Services />
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar';
+import TeamCard from './components/TeamCard'
+import Services from "./components/Services"
+export default {
+  name: 'App',
+  components: {
+    Services,
+    TeamCard,
+    NavBar
+  }
+}
+</script>
+
