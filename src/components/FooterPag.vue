b8383340e5643cd915a0315a807d447923197373
Viviana Chacon <apple@MacBook-Pro-de-Apple.local>
2020-12-05 23:15:25 -0500

    add member.image

--- src/components/FooterPag.vue
+++ src/components/FooterPag.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="row ml-5 mr-auto">
-        <div class="col-md-5">
+    <div class="row ml-6 mr-auto" style="margin-left: 15px">
+        <div class="col-md-6">
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Grupo 88 Sprint 2</h5>
-                    <p class="card-text text-justify"><br>El motivo de este desarrollo es aplicar los conocimientos aprendidos en las clases de Misión TIC 2022, utilizando Bootstrap, con la finalidad de poder trabajar mejor en equipo y desarrollar nuestras habilidades de programación. Practicar el trabajo colaboorativos desde GitHub y aplicar  los conocimentos adquiridos de Vue CLI construyendo un sitio con características mas profesionales.<br><br></p>
+                    <p class="card-text text-justify">El motivo de este desarrollo es aplicar los conocimientos aprendidos en las clases de Misión TIC 2022, utilizando Bootstrap, con la finalidad de poder trabajar mejor en equipo y desarrollar nuestras habilidades de programación. Practicar el trabajo colaboorativos desde GitHub y aplicar  los conocimentos adquiridos de Vue CLI construyendo un sitio con características mas profesionales.</p>
                     <p>
                         Link del repositorio: <br>
                         <a href="https://github.com/carlosescor2199/semana-1-88">
